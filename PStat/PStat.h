bc8da72a0baa329e8d5b1a3bdd72863c4fee6d55
media-pt <media-pt@mac-mini-media-pt.local>
2016-11-07 15:52:38 +0200

    Initial Commit

---
+++ PStat/PStat.h
@@ -0,0 +1,19 @@
+//
+//  PStat.h
+//  PStat
+//
+//  Created by media-pt on 07.11.16.
+//  Copyright © 2016 media-pt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PStat.
+FOUNDATION_EXPORT double PStatVersionNumber;
+
+//! Project version string for PStat.
+FOUNDATION_EXPORT const unsigned char PStatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PStat/PublicHeader.h>
+
+
